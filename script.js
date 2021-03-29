768ae4ecd87eab335cc3d72bf0279a91a39e9214
opensourcifier <64643552+opensourcifier@users.noreply.github.com>
2021-03-29 21:16:53 +0530

    Remove jQuery

--- script.js
+++ script.js
@@ -2,52 +2,54 @@
 AOS.init();
 
 // Remove loader on page load
-$(window).on("load", () => {
-    $(".loader-container").fadeOut();
+let loader = document.getElementsByClassName("loader-container")[0];
+window.onload = () => {
+    loader.classList.add("loaded");
+};
+loader.addEventListener("animationend", () => {
+    loader.style.display = "none";
 });
 
-// Hamburger menu
-const hamburgerButton = $(".navbar__menu-btn");
-const navbarLinks = $(".nav__links");
+// Navigation bar menu button
+const menuBtn = document.getElementsByClassName("navbar__menu-btn")[0];
+const navbarLinks = document.getElementsByClassName("nav__links")[0];
 
-$(hamburgerButton).on("click", () => {
-    $(navbarLinks).toggleClass("active");
-    if ($(navbarLinks).hasClass("active")) {
-        $(hamburgerButton).html('<i class="fas fa-times-circle fa-fw"></i>');
+menuBtn.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+    if (navbarLinks.classList.contains("active")) {
+        menuBtn.innerHTML = '<i class="fas fa-times-circle fa-fw"></i>';
     } else {
-        $(hamburgerButton).html('<i class="fas fa-bars fa-fw"></i>');
+        menuBtn.innerHTML = '<i class="fas fa-bars fa-fw"></i>';
     }
 });
 
 // Dark Mode
 let darkMode = localStorage.getItem("darkMode");
-const darkModeToggle = $("#dark-mode-toggle");
-const imgReplace = $("#thumbnail-replace-dark");
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const imgReplace = document.getElementById("thumbnail-replace-dark");
 
 function enableDarkMode() {
     // Add class "darkmode" to <body>
-    $("body").addClass("darkmode");
+    document.body.classList.add("darkmode");
     // Update darkMode in localStorage
     localStorage.setItem("darkMode", "enabled");
     // Change GitHub Pages thumbnail
-    $(imgReplace).attr("src", "/images/ghpages-dark.png");
+    imgReplace.src = "/images/ghpages-dark.png";
     // Change moon icon to sun icon
-    $(darkModeToggle).html(
-        '<i class="fas fa-sun fa-fw"></i><p> Light Mode</p>'
-    );
+    darkModeToggle.innerHTML =
+        '<i class="fas fa-sun fa-fw"></i><p> Light Mode</p>';
 }
 
 function disableDarkMode() {
     // Remove class "darkmode" from <body>
-    $("body").removeClass("darkmode");
+    document.body.classList.remove("darkmode");
     // Update darkMode in localStorage
     localStorage.setItem("darkMode", null);
     // Change GitHub Pages thumbnail
-    $(imgReplace).attr("src", "/images/ghpages.png");
+    imgReplace.src = "/images/ghpages.png";
     // Change sun icon to moon icon
-    $(darkModeToggle).html(
-        '<i class="fas fa-moon fa-fw"></i><p> Dark Mode</p>'
-    );
+    darkModeToggle.innerHTML =
+        '<i class="fas fa-moon fa-fw"></i><p> Dark Mode</p>';
 }
 
 if (darkMode === "enabled") {
@@ -73,5 +75,5 @@ function darkModeSwitcher() {
 }
 
 // Dark mode toggle button event listeners
-$(darkModeToggle).click(darkModeSwitcher);
-$(darkModeToggle).keypress(darkModeSwitcher);
+darkModeToggle.addEventListener("click", darkModeSwitcher);
+darkModeToggle.addEventListener("keypress", darkModeSwitcher);
